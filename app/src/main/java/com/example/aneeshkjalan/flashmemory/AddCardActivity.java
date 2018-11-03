80ac49b90b0662f35b768cbe348e7d9e63bc9dd7
AneeshJalan <akjalan@ucsd.edu>
2018-11-02 22:11:28 -0700

    Added feature to create a flashcard with custom Question and Answer

---
+++ app/src/main/java/com/example/aneeshkjalan/flashmemory/AddCardActivity.java
@@ -0,0 +1,47 @@
+package com.example.aneeshkjalan.flashmemory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    public final int CREATE_QUESTION_CODE = 1;
+
+    EditText newQuestion;
+    EditText newAnswer;
+    ImageButton cancelButton;
+    ImageButton saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        newQuestion = findViewById(R.id.new_question);
+        newAnswer = findViewById(R.id.new_answer);
+        cancelButton = findViewById(R.id.cancel_question_creation);
+        saveButton = findViewById(R.id.save_question);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent updateFlashcard = new Intent(AddCardActivity.this, MainActivity.class);
+                updateFlashcard.putExtra("question", newQuestion.getText());
+                updateFlashcard.putExtra("answer", newAnswer.getText());
+                setResult(RESULT_OK, updateFlashcard );
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/aneeshkjalan/flashmemory/MainActivity.java
+++ app/src/main/java/com/example/aneeshkjalan/flashmemory/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.aneeshkjalan.flashmemory;
 
+import android.content.Intent;
+import android.service.autofill.TextValueSanitizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final int CREATE_QUESTION_CODE = 1;
+
+    private TextView flashcardQuestion;
+    private TextView flashcardAnswer;
+    private ImageButton addQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+        flashcardQuestion = findViewById(R.id.flashcard_question);
+        flashcardAnswer = findViewById(R.id.flashcard_answer);
+        addQuestion = findViewById(R.id.add_question);
+
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
             }
-
         });
+
+        addQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createQuestion = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(createQuestion,1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == CREATE_QUESTION_CODE) {
+            flashcardQuestion.setText(data.getCharSequenceExtra("question"));
+            flashcardAnswer.setText(data.getCharSequenceExtra("answer"));
+            flashcardQuestion.setVisibility(View.VISIBLE);
+            flashcardAnswer.setVisibility(View.INVISIBLE);
+        }
     }
 
 
